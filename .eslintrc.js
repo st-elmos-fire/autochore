fc13c0d8d7efabf91a312814d5cac938dea8e67e
Alex Foxleigh <alex@alexfoxleigh.com>
2022-02-19 12:44:12 +0000

    Adds linting to the project

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'next'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      env: {
+        browser: true
+      },
+      extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'eslint-config-prettier'
+      ]
+    }
+  ],
+  env: {
+    browser: true,
+    node: true
+  },
+  ignorePatterns: ['**/venv/*'],
+  rules: {
+    'react/prop-types': [2, { ignore: ['children'] }]
+  }
+};
