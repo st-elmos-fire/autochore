beb98fba7aa3ac08963b09c7aa1fc1d97db27d71
Alex Foxleigh <alex@alexfoxleigh.com>
2022-02-19 09:22:43 +0000

    created new app-header component

---
+++ components/app-header/index.test.tsx
@@ -0,0 +1,17 @@
+/* global it expect */
+
+import React from 'react'
+import { render } from '@testing-library/react';
+
+// Import component files
+import AppHeader from './index'
+
+// Tests
+describe('AppHeader component', () => {
+  it('renders to match snapshot', () => {
+    const { baseElement } = render(
+      <AppHeader name="test" colour="blue" />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+});
