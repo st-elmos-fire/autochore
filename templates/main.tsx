bef1fbfe14dfbfff5f1228f15be30755dc6fffc8
Alex Foxleigh <alex@alexfoxleigh.com>
2022-02-19 08:43:03 +0000

    removed drawer from main and replaced with side-navigation component

--- templates/main.tsx
+++ templates/main.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
 import Head from 'next/head';
-import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { ListAlt, Task } from '@mui/icons-material';
 import { useContext, useState } from 'react';
 import { ModeContext } from '../pages';
+import SideNavigation from '../components/side-navigation';
 
 const drawerWidth: number = 240;
 
@@ -41,32 +37,6 @@ const AppBar = styled(MuiAppBar, {
     }),
   }),
 }));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-  );
   
   const mdTheme = createTheme();
 
@@ -82,84 +52,51 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     
   const { dispatch } = useContext(ModeContext);
   
-
   return (
-      <ThemeProvider theme={mdTheme}>
-        <Head>
-          <title>Chores CMS</title>
-          <meta name="description" content="Add your chores here!" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-            <Box sx={{ display: 'flex' }}>
-              <CssBaseline />
-              <AppBar position="absolute" open={open}>
-                <Toolbar sx={{pr: '24px'}}>
-                  <IconButton edge="start" color="inherit" aria-label="open drawer"
-                    onClick={toggleDrawer}
-                    sx={{
-                      marginRight: '36px',
-                      ...(open && { display: 'none' }),
-                    }}
-                    >
-                    <MenuIcon />
-                  </IconButton>
-                  <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-                    Chores CMS
-                  </Typography>
-                </Toolbar>
-              </AppBar>
-              <Drawer variant="permanent" open={open}>
-                <Toolbar
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                    px: [1],
-                  }}
-                  >
-                  <IconButton onClick={toggleDrawer}>
-                    <ChevronLeftIcon />
-                  </IconButton>
-                </Toolbar>
-                <Divider />
-                <List>
-                  <ListItem disablePadding>
-                    <ListItemButton onClick={(e) => dispatch('VIEW')}>
-                      <ListItemIcon>
-                        <ListAlt />
-                      </ListItemIcon>
-                      <ListItemText primary="View chores list" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton onClick={(e) => dispatch('ADD')}>
-                      <ListItemIcon>
-                        <Task />
-                      </ListItemIcon>
-                      <ListItemText primary="Add new chore  " />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </Drawer>
-              <Box
-                component="main"
-                sx={{
-                  backgroundColor: (theme) =>
-                  theme.palette.mode === 'light'
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-                  flexGrow: 1,
-                  height: '100vh',
-                  overflow: 'auto',
-                }}
-                >
-                <Toolbar />
-                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                    {children}
-                </Container>
-              </Box>
-            </Box>
-      </ThemeProvider>
+    <ThemeProvider theme={mdTheme}>
+      <Head>
+        <title>Chores CMS</title>
+        <meta name="description" content="Add your chores here!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar sx={{pr: '24px'}}>
+            <IconButton edge="start" color="inherit" aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+              >
+              <MenuIcon />
+            </IconButton>
+            <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+              Chores CMS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <SideNavigation open={open} dispatch={dispatch} toggleDrawer={toggleDrawer} drawerWidth={drawerWidth} />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+          >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              {children}
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
